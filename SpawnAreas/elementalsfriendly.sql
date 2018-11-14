e8b1c0daff851995c7aa3a1874412849553cf11e
Bokrom <zaksfreeman@gmail.com>
2018-11-14 12:03:01 -0500

    Correct elemental faction

--- SpawnAreas/elementalsfriendly.sql
+++ SpawnAreas/elementalsfriendly.sql
@@ -1,3 +1,3 @@
 UPDATE creature_template
-SET faction_A = '0', faction_H = '0', speed_walk = '0.8'
+SET faction_A = '35', faction_H = '31', speed_walk = '0.8'
 WHERE entry IN ('2761', '2762', '2760', '2592');
