c4482e7ce08b201da6fe01403d2bcbe849d624fe
Bokrom <zaksfreeman@gmail.com>
2018-11-15 23:45:30 -0500

    Eles and trainers

--- spawnarea/elementalsfriendly.sql
+++ spawnarea/elementalsfriendly.sql
@@ -2,6 +2,9 @@ UPDATE creature_template
 SET faction_A = '35', faction_H = '31', speed_walk = '0.8'
 WHERE entry IN ('2761', '2762', '2760', '2592');
 
-# Remove an elemental that's on top of Shaman spawn point
+# Remove creatures near
+# Shaman spawn point
+# Mage and Warlock trainers
+# Priest, Druid, Shaman trainers
 DELETE FROM creature
-WHERE guid = '11754';
+WHERE guid IN ('11754', '14505', '12041');
